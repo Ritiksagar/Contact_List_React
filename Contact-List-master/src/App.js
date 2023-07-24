dd88d93122cc25c8a9a015b2063c3c586c984e47
Ritiksagar <97246933+Ritiksagar@users.noreply.github.com>
2023-07-24 18:51:16 +0530

    Add files via upload

---
+++ Contact-List-master/src/App.js
@@ -0,0 +1,12 @@
+import ContactList from "./components/ContactList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CONTACT LIST</h1>
+      <ContactList/>  
+    </div>
+  );
+}
+
+export default App;
